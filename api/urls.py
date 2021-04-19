c94cded40d52b7cb52a0fd2e90420cc76030023c
60070106 <60070106@kmitl.ac.th>
2021-04-19 14:05:25 +0700

    change api url for faster response

--- api/urls.py
+++ api/urls.py
@@ -7,15 +7,15 @@ urlpatterns = [
     path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     # path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
-    path('event/register/', EventView.as_view(), name='event_register'),
-    path('event/get_all_event/', GetAllEventView.as_view(), name='event_all'),
-    path('event/get/detail/', GetEventDetail.as_view(), name='event_detail'),
-    path('event/camp/register/', RegisterEvent.as_view(), name='camp_register'),
-    path('event/camp/people/', GetEventPeople.as_view(), name='camp_people'),
-    path('event/camp/attendance/', GetEventAvaliableCheckin.as_view(), name='camp_attendance'),
-    path('event/camp/check/', EventCheckView.as_view(), name='camp_checkin'),
-    path('event/approver/check/', EventApproverView.as_view(), name='approver_event'),
-    path('event/edit/', EditEventDetail.as_view(), name='event_edit')
+    path('event_register/', EventView.as_view(), name='event_register'),
+    path('get_all_event/', GetAllEventView.as_view(), name='event_all'),
+    path('event_get_detail/', GetEventDetail.as_view(), name='event_detail'),
+    path('event_camp_register/', RegisterEvent.as_view(), name='camp_register'),
+    path('event_camp_people/', GetEventPeople.as_view(), name='camp_people'),
+    path('event_camp_attendance/', GetEventAvaliableCheckin.as_view(), name='camp_attendance'),
+    path('event_camp_check/', EventCheckView.as_view(), name='camp_checkin'),
+    path('event_approver_check/', EventApproverView.as_view(), name='approver_event'),
+    path('event_edit/', EditEventDetail.as_view(), name='event_edit')
 ]
 
 # GetAllEventView
